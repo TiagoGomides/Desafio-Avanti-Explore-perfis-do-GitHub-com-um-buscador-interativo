cc251639f39e099f5857cd80a6e3870a8a4ba3d9
TiagoGomides <gomidestiago@outlook.com>
2025-04-20 17:38:54 -0300

    :art: style(User): Nomear classes CSS no componente user.tsx
    As classes CSS foram nomeadas de forma padronizada e organizadas no componente user.tsx para facilitar o uso com CSS Modules e melhorar a manutenção do código.

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,4 +1,5 @@
 import { UserProps } from "../types/user";
+import styles from './User.module.css'
 
 
 const User = ({
@@ -8,13 +9,15 @@ const User = ({
 }: UserProps) => {
 
     return (
-        <section >
-            <img src={avatar_url} alt={login} />
-            <h2>{login}</h2>
-            {bio && (
-                <p>{bio}</p>
-            )
-            }
+        <section className={styles.container}>
+            <img src={avatar_url} alt={login} className={styles.img} />
+            <div className={styles.content}>
+                <h2>{login}</h2>
+                {bio && (
+                    <p>{bio}</p>
+                )
+                }
+            </div>
         </section>
     )
 }
