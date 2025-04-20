681a1280845af56e055714b139bc70918cce8627
TiagoGomides <gomidestiago@outlook.com>
2025-04-20 13:19:54 -0300

    :sparkles: feat(search): Permitir pesquisa ao pressionar Enter
    Adicionada funcionalidade que ativa a busca ao pressionar a tecla Enter no campo de pesquisa, melhorando a usabilidade e fluidez da interação do usuário.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,18 +2,26 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
 
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 import { BsSearch } from "react-icons/bs"
 import styles from './Search.module.css'
 
 const Search = ({ loadUser }: SearchProps) => {
     const [userName, setUserName] = useState("");
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+            loadUser(userName);
+        }
+    }
+
 
     return (
         <section className={styles.container}>
             <input type="text"
                 placeholder="Digite um usuário do Github"
-                onChange={(e) => setUserName(e.target.value)} />
+                onChange={(e) => setUserName(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
             <button onClick={() => loadUser(userName)}>
                 <BsSearch />
             </button>
