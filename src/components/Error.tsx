3088b1139606a428aaa27cbe0d8348ceba89c841
TiagoGomides <gomidestiago@outlook.com>
2025-04-20 13:59:39 -0300

    :boom: feat(ErrorMessage): Criar componente para exibição de erros
    Componente dedicado à exibição de mensagens de erro para feedback ao usuário, especialmente em casos de falha na busca de usuários ou problemas de rede.

---
+++ src/components/Error.tsx
@@ -0,0 +1,14 @@
+
+
+const Error = () => {
+    return (
+        <section>
+            <p>
+                Nenhum perfil foi encontrado com ese nome de usuário.
+                Tente novamente
+            </p>
+        </section>
+    )
+}
+
+export default Error;
