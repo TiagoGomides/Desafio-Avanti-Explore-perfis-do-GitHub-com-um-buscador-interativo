79896632a24912277078c760c69812ca085dba82
TiagoGomides <gomidestiago@outlook.com>
2025-04-20 13:23:03 -0300

    :sparkles: feat(home): Integrar busca de usuário do GitHub com fetch
    A página Home agora permite buscar dados de usuários do GitHub utilizando a API oficial via fetch, exibindo os resultados diretamente na interface.

--- src/router/Home.tsx
+++ src/router/Home.tsx
@@ -1,8 +1,36 @@
+import { UserProps } from "../types/user";
 import React from "react";
+import Search from "../components/Search";
+import { useState } from "react";
 
-const Home =()=>{
-    return(
-        <div>Home</div>
+
+
+
+
+const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async (userName: string) => {
+
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await res.json();
+
+        const {avatar_url, login, bio} = data;
+
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            bio
+        }
+
+        setUser(userData)
+    }
+
+    return (
+        <section>
+            <Search loadUser={loadUser} />
+            {user&& <p>{user.login}</p>}
+        </section>
     )
 }
 
