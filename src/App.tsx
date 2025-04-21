33891b2321bbad565be0283b57b032fbcebbf9bb
TiagoGomides <gomidestiago@outlook.com>
2025-04-21 13:39:10 -0300

    :sparkles: feat(App): Adicionar novos elementos ao componente App.tsx
    Foram inseridos novos elementos no App.tsx com o objetivo de estruturar melhor a aplicação e integrar componentes essenciais para o funcionamento da interface.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom"
-import logoGitHub from "./assets/svg/githubLogo.svg"
+import { Outlet } from "react-router-dom";
+import logoGitHub from "./assets/svg/githubLogo.svg";
+import styles from "./App.module.css"
 function App() {
 
   return (
     <>
-      <main className="container">
-
-        <section className="flex-row">
+      <main className={styles.container}>
+        <div className={styles.flexRow}>
           <img src={logoGitHub} alt="logoGitHub" width={60} />
-          <h1 className="pd8"> Perfil <strong>GitHub</strong></h1>
-        </section>
+          <h1 className={styles.pd8}> Perfil <strong>GitHub</strong></h1>
+        </div>
         <Outlet />
       </main>
 
